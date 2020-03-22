7451d08c8b2b6cb1677d01a615a75861587372da
Leon Stoldt <leon.stoldt.a17b@nordakademie.org>
2020-03-22 20:19:27 +0100

    fixed missing root in jdbc url and changed path in startup.sh

--- src/main/resources/startup.sh
+++ src/main/resources/startup.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 sleep 5
-xrandr -s 1024x600 &
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y git maven default-jre midori matchbox unclutter
+sudo apt-get install -y git maven midori x11-xserver-utils matchbox unclutter ca-certificates-java java-common
 
 cd "$HOME" || exit
-
-LOVEBOX_DIR=$HOME/Lovebox
+LOVEBOX_DIR=$HOME/Desktop/Lovebox
 
 if [ ! -d "$LOVEBOX_DIR" ]
 then
     echo "Directory $LOVEBOX_DIR DOES NOT exist."
+    cd Desktop || exit
     git clone https://github.com/LeonStoldt/Lovebox.git
 fi
 
+# start lovebox program
 cd LOVEBOX_DIR || exit
-
 git pull
 mvn clean install
-mvn spring-boot:run
+mvn spring-boot:run &> logs/application.log &
+
+export DISPLAY=:0.0
 
 xset -dpms # disable DPMS (Energy Star) features.
 xset s off # disable screen saver
 xset s noblank # don't blank the video device
+
+# start browser in fullscreen (kiosk mode) and remove mouse cursor
 unclutter &
-matchbox-window-manager -use_cursor no -use_titlebar no  &
+matchbox-window-manager -use_cursor no -use_titlebar no &
 while :; do
   ps -ef | grep 'midori -e Fullscreen' | grep -v grep > /dev/null 2>&1
   if [ $? -eq 1 ] ; then
