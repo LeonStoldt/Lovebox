49103731fbd6b38ef59bae4eb1fb83fbfe0b10ee
Leon Stoldt <leon.stoldt.a17b@nordakademie.org>
2020-03-22 22:14:34 +0100

    fixed wrong path of script

--- src/main/resources/autostart-lovebox.sh
+++ src/main/resources/autostart-lovebox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # copy this file to an autostart directory
 sleep 5
-sudo xinit "$HOME/Desktop/Lovebox/src/main/resources/lovebox.sh"
+sudo xinit "$HOME"/Desktop/Lovebox/src/main/resources/lovebox.sh
