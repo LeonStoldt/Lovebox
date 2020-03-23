cd011c260562acfaecc3a0df558f099950fe17bc
Leon Stoldt <leon.stoldt.a17b@nordakademie.org>
2020-03-23 20:43:45 +0100

    added output to script and made autostart script logging to file

--- src/main/resources/autostart-lovebox.sh
+++ src/main/resources/autostart-lovebox.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# copy this file to an autostart directory
-sleep 5
-sudo xinit "$HOME"/Desktop/Lovebox/src/main/resources/lovebox.sh
+# 1. copy this content to custom.sh in /var/lib/dietpi/dietpi-autostart/
+# 2. setup custom.sh as startup script in dietpi-config -> autostart options
+sleep 3
+sudo /root/Desktop/Lovebox/src/main/resources/lovebox.sh |& tee /var/lib/dietpi/dietpi-autostart/lovebox.log
--- src/main/resources/lovebox.sh
+++ src/main/resources/lovebox.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y git maven midori x11-xserver-utils matchbox unclutter ca-certificates-java java-common
+pkgs='git maven midori x11-xserver-utils matchbox unclutter ca-certificates-java java-common at-spi2-core'
+if ! dpkg -s "$pkgs" >/dev/null 2>&1; then
+  echo "Installing required packages."
+  sudo apt-get install -y "$pkgs"
+fi
+echo "All packages are installed."
 
 cd "$HOME" || exit
 LOVEBOX_DIR=$HOME/Desktop/Lovebox
@@ -10,27 +14,34 @@ if [ ! -d "$LOVEBOX_DIR" ]
 then
     echo "Directory $LOVEBOX_DIR DOES NOT exist."
     cd Desktop || exit
+    echo "Cloning the repository..."
     git clone https://github.com/LeonStoldt/Lovebox.git
 fi
 
-# start lovebox program
 cd "$LOVEBOX_DIR" || exit
+echo "Moved to Lovebox Directory."
 git pull
 mvn clean compile
+echo "Starting Lovebox Application. Log of application: /LOVEBOX_DIR/logs/application.log"
 mvn spring-boot:run &> logs/application.log &
 
 export DISPLAY=:0.0
 
-xset -dpms # disable DPMS (Energy Star) features.
-xset s off # disable screen saver
-xset s noblank # don't blank the video device
+echo "Disable DPMS (Energy Star) features."
+xset -dpms
+echo "Disable screen saver."
+xset s off
+echo "Don't blank the video device."
+xset s noblank
 
-# start browser in fullscreen (kiosk mode) and remove mouse cursor
+echo "Removing mouse cursor."
 unclutter &
+echo "Starting Window Manager."
 matchbox-window-manager -use_cursor no -use_titlebar no &
 while :; do
   ps -ef | grep 'midori -e Fullscreen' | grep -v grep > /dev/null 2>&1
   if [ $? -eq 1 ] ; then
+    echo "Startting Midori in Fullscreen mode."
     midori -e Fullscreen -a http://localhost &
   fi
   sleep 1800
