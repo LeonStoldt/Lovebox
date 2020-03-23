a2a7b560859a8d754c62a50dc150bb6237827bbf
Leon Stoldt <leon.stoldt.a17b@nordakademie.org>
2020-03-23 21:24:19 +0100

    only installing packages if not already installed and source script to export DISPLAY

--- src/main/resources/autostart-lovebox.sh
+++ src/main/resources/autostart-lovebox.sh
@@ -2,4 +2,4 @@
 # 1. copy this content to custom.sh in /var/lib/dietpi/dietpi-autostart/
 # 2. setup custom.sh as startup script in dietpi-config -> autostart options
 sleep 3
-sudo /root/Desktop/Lovebox/src/main/resources/lovebox.sh |& tee /var/lib/dietpi/dietpi-autostart/lovebox.log
+source /root/Desktop/Lovebox/src/main/resources/lovebox.sh |& tee /var/lib/dietpi/dietpi-autostart/lovebox.log
--- src/main/resources/lovebox.sh
+++ src/main/resources/lovebox.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 sudo apt-get update
-pkgs='git maven midori x11-xserver-utils matchbox unclutter ca-certificates-java java-common at-spi2-core'
-if ! dpkg -s "$pkgs" >/dev/null 2>&1; then
-  echo "Installing required packages."
-  sudo apt-get install -y "$pkgs"
-fi
-echo "All packages are installed."
+
+packages=(git maven midori x11-xserver-utils matchbox unclutter ca-certificates-java java-common at-spi2-core)
+for package in "${packages[@]}"; do
+  if [ "$(dpkg-query -W -f='${Status}' "$package" 2>/dev/null | grep -c "ok installed")" -eq 0 ]; then
+    echo "Installing required package: $package"
+    apt-get install "$package"
+  fi
+done
 
 cd "$HOME" || exit
 LOVEBOX_DIR=$HOME/Desktop/Lovebox
 
-if [ ! -d "$LOVEBOX_DIR" ]
-then
-    echo "Directory $LOVEBOX_DIR DOES NOT exist."
-    cd Desktop || exit
-    echo "Cloning the repository..."
-    git clone https://github.com/LeonStoldt/Lovebox.git
+if [ ! -d "$LOVEBOX_DIR" ]; then
+  echo "Directory $LOVEBOX_DIR DOES NOT exist."
+  cd Desktop || exit
+  echo "Cloning the repository..."
+  git clone https://github.com/LeonStoldt/Lovebox.git
 fi
 
 cd "$LOVEBOX_DIR" || exit
@@ -23,8 +24,9 @@ echo "Moved to Lovebox Directory."
 git pull
 mvn clean compile
 echo "Starting Lovebox Application. Log of application: /LOVEBOX_DIR/logs/application.log"
-mvn spring-boot:run &> logs/application.log &
+mvn spring-boot:run &>logs/application.log &
 
+echo "Exporting DISPLAY"
 export DISPLAY=:0.0
 
 echo "Disable DPMS (Energy Star) features."
@@ -39,8 +41,8 @@ unclutter &
 echo "Starting Window Manager."
 matchbox-window-manager -use_cursor no -use_titlebar no &
 while :; do
-  ps -ef | grep 'midori -e Fullscreen' | grep -v grep > /dev/null 2>&1
-  if [ $? -eq 1 ] ; then
+  ps -ef | grep 'midori -e Fullscreen' | grep -v grep >/dev/null 2>&1
+  if [ $? -eq 1 ]; then
     echo "Startting Midori in Fullscreen mode."
     midori -e Fullscreen -a http://localhost &
   fi
